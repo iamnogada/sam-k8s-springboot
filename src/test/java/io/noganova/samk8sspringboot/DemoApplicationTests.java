19f0ad08dea106f6497225629e6a3c107c46decf
myguddy <seungil.han@gmail.com>
2018-10-25 16:58:00 +0900

    init project

---
+++ src/test/java/io/noganova/samk8sspringboot/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package io.noganova.samk8sspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
